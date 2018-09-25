b0215d3859b4aacd548e8dc3e8c4212fecfa5717
rgboss12 <rohangarg12@gmail.com>
2018-09-26 01:28:22 +0530

    Added navigation to Home Page

--- main.dart
+++ main.dart
@@ -11,14 +11,7 @@ class MyApp extends StatelessWidget {
     return new MaterialApp(
       title: 'Flutter Demo',
       theme: new ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or press Run > Flutter Hot Reload in IntelliJ). Notice that the
-        // counter didn't reset back to zero; the application is not restarted.
+
         primarySwatch: Colors.blue,
       ),
       home: new MyLoginPage(title: 'Login'),
@@ -26,17 +19,29 @@ class MyApp extends StatelessWidget {
   }
 }
 
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key, key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context){
+    return Scaffold (
+      appBar : AppBar(
+        title: Text('Home Page'),
+
+      ),
+      body: Text('Hello and fuck off'),
+    );
+  }
+}
+
 class MyLoginPage extends StatefulWidget {
   MyLoginPage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -45,18 +50,7 @@ class MyLoginPage extends StatefulWidget {
 }
 
 class _MyLoginPageState extends State<MyLoginPage> {
-//  int _counter = 0;
-//
-//  void _incrementCounter() {
-//    setState(() {
-//      // This call to setState tells the Flutter framework that something has
-//      // changed in this State, which causes it to rerun the build method below
-//      // so that the display can reflect the updated values. If we changed
-//      // _counter without calling setState(), then the build method would not be
-//      // called again, and so nothing would appear to happen.
-//      _counter++;
-//    });
-//  }
+
   final GlobalKey<FormState> _formKey = new GlobalKey<FormState>();
   String username = '';
   String password = '';
@@ -110,7 +104,7 @@ class _MyLoginPageState extends State<MyLoginPage> {
       form.save();
       print('Username: $username');
       print('Password: $password');
-      session.post('http://192.168.2.11:8080/outlab8/LoginServlet',
+      session.post('http://10.196.5.236:8080/outlab8/LoginServlet',
           {"userid": username, "password": password}).then(_resToAuth);
     }
   }
@@ -118,6 +112,7 @@ class _MyLoginPageState extends State<MyLoginPage> {
   void _resToAuth(s) {
     Map<String, dynamic> response = json.decode(s);
     if (response['status']) {
+      Navigator.push(context, MaterialPageRoute(builder: (context) => MyHomePage()));
       print('Authorised');
     } else {
       print('Authorisation failed');
